fb1024f2450f5ad49fd5f28dc34e7e6e711e75b1
fynn.kroeger <fynn_kroeger@web.de>
2024-06-01 15:32:18 +0200

    refactor to strategy for formatting

--- inference.py
+++ inference.py
@@ -9,8 +9,10 @@ import string
 
 
 def translate(
+    test,
     lang_pair,
     n_shots,
+    prompt_formatter,
     few_shot_dataset_name,
     test_dataset_name,
     out_dir,
@@ -20,8 +22,7 @@ def translate(
     batch_size,
 ):
     source_lang, target_lang = lang_pair.split("-")
-    prompt = f"Translate this from {LANG_TABLE[source_lang]} to {LANG_TABLE[target_lang]}. Respond only with the translation.\n"
-    one_message = 0
+    source_lang, target_lang = LANG_TABLE[source_lang], LANG_TABLE[target_lang]
 
     if few_shot_dataset_name and n_shots:
         with open(f"datasets/{few_shot_dataset_name}_{lang_pair}.json") as f:
@@ -29,30 +30,12 @@ def translate(
 
         random.seed(42)
         random.shuffle(few_shot_dataset)
+        few_shot_examples = few_shot_dataset[:n_shots]
 
-        if one_message == 1:
-            few_shot_prompt_string = ""
-            for sample in few_shot_dataset[:n_shots]:
-                few_shot_prompt_string += f"{sample['source']} -> {sample['target']}\n"
-            print(few_shot_prompt_string)
-        elif one_message == 2:
-            few_shot_prompt_string = ""
-            for sample in few_shot_dataset[:n_shots]:
-                few_shot_prompt_string += f"{LANG_TABLE[source_lang]}: {sample['source']}\n{LANG_TABLE[target_lang]}: {sample['target']}\n\n"
-            print(few_shot_prompt_string)
-        else:
-            few_shot_prompt_messages = []
-            for sample in few_shot_dataset[:n_shots]:
-                few_shot_prompt_messages.append(
-                    dict(role="user", content=prompt + sample["source"])
-                )
-                few_shot_prompt_messages.append(dict(role="assistant", content=sample["target"]))
-
-        out_path = out_dir / f"{test_dataset_name}_{lang_pair}_{n_shots}shot_{few_shot_dataset_name}{'_'+str(one_message) if one_message else ''}_{run_name}.json"
+        out_path = out_dir / f"{test_dataset_name}_{lang_pair}_{n_shots:02d}shot_{few_shot_dataset_name}_{run_name}.json"
     else:
-        one_message = 0
-        few_shot_prompt_messages = []
-        out_path = out_dir / f"{test_dataset_name}_{lang_pair}_0shot_{run_name}.json"
+        few_shot_examples = []
+        out_path = out_dir / f"{test_dataset_name}_{lang_pair}_00shot_{run_name}.json"
     print(f"writing output to {out_path} at the end")
     out_dir.mkdir(exist_ok=True)
 
@@ -63,28 +46,8 @@ def translate(
         batch = test_dataset[i : i + batch_size]
         messages2d = []
         for sample in batch:
-            if one_message == 1:
-                messages2d.append(
-                    [
-                        {
-                            "role": "user",
-                            "content": few_shot_prompt_string + sample["source"] + " -> ",
-                        }
-                    ]
-                )
-            elif one_message == 2:
-                messages2d.append(
-                    [
-                        {
-                            "role": "user",
-                            "content": few_shot_prompt_string
-                            + f"{LANG_TABLE[source_lang]}: {sample['source']}\n{LANG_TABLE[target_lang]}: ",
-                        }
-                    ]
-                )
-            else:
-                sample_prompt_message = [{"role": "user", "content": prompt + sample["source"]}]
-                messages2d.append(few_shot_prompt_messages + sample_prompt_message)
+            formatted = prompt_formatter(few_shot_examples, sample["source"], source_lang, target_lang)
+            messages2d.append(formatted)
         if i == 0:
             print(prompt_log := messages2d[0])
         model_inputs = tokenizer.apply_chat_template(
@@ -95,9 +58,8 @@ def translate(
             add_generation_prompt=True,
         )
         input_sequence_len = model_inputs.shape[1]
-        model_inputs = model_inputs.to("cuda")
         generation = model.generate(
-            model_inputs,
+            model_inputs.to("cuda"),
             max_new_tokens=512,
             pad_token_id=tokenizer.eos_token_id,
             return_dict_in_generate=True,
@@ -115,18 +77,62 @@ def translate(
                     translation=translation,
                 )
             )
+        if test:
+            break
 
-    with open(out_path, "w") as f:
-        json.dump(output, f, indent=1)
+    if not test:
+        with open(out_path, "w") as f:
+            json.dump(output, f, indent=1)
 
-    # todo what to log, what into filename
-    logs = {}
-    if (log_file := Path("outputs/logs.json")).exists():
-        logs = json.loads(log_file.read_text())
-    logs[out_path.name] = dict(prompt=prompt_log)
-    with open(log_file, "w") as f:
-        sorted_logs = dict(sorted([(k, v) for k, v in logs.items()]))
-        json.dump(sorted_logs, f, indent=1)
+        # todo what to log, what into filename
+        logs = {}
+        if (log_file := Path("outputs/logs.json")).exists():
+            logs = json.loads(log_file.read_text())
+        logs[out_path.name] = dict(prompt=prompt_log)
+        with open(log_file, "w") as f:
+            sorted_logs = dict(sorted([(k, v) for k, v in logs.items()]))
+            json.dump(sorted_logs, f, indent=1)
+
+
+def format_multi_message(few_shot_examples, source, source_lang, target_lang):
+    prompt = f"Translate this from {source_lang} to {target_lang}. Respond only with the translation.\n"
+    messages = []
+    for sample in few_shot_examples:
+        messages.append(
+            dict(role="user", content=prompt + sample["source"])
+        )
+        messages.append(dict(role="assistant", content=sample["target"]))
+    return messages+[{"role": "user", "content": prompt+source}]
+
+
+def format_single_message_prompt_arrow(few_shot_examples, source, source_lang, target_lang):
+    if few_shot_examples:
+        few_prompt = f"Example translations from {source_lang} to {target_lang}:\n"
+        for sample in few_shot_examples:
+            few_prompt += f"{sample['source']} -> {sample['target']}\n"
+        few_prompt += "\n"
+    else:
+        few_prompt = ""
+    # consider / using examples, ...
+    prompt = f"Translate this from {source_lang} to {target_lang}. Respond only with the translation.\n"
+    return [{"role": "user", "content": few_prompt+prompt+source}]
+
+
+def format_single_message_arrow(few_shot_examples, source, source_lang, target_lang):
+    if not few_shot_examples:
+        raise NotImplementedError()
+    few_prompt = ""
+    for sample in few_shot_examples:
+        few_prompt += f"{sample['source']} -> {sample['target']}\n"
+    return [{"role": "user", "content": few_prompt+f"{source} -> "}]
+
+
+def format_single_message_labeled(few_shot_examples, source, source_lang, target_lang):
+    few_prompt = ""
+    for sample in few_shot_examples:
+        few_prompt += f"{LANG_TABLE[source_lang]}: {sample['source']}\n{LANG_TABLE[target_lang]}: {sample['target']}\n\n"
+    prompt = f"{LANG_TABLE[source_lang]}: {source}\n{LANG_TABLE[target_lang]}: "
+    return [{"role": "user", "content": few_prompt+prompt}]
 
 
 if __name__ == "__main__":
@@ -137,17 +143,21 @@ if __name__ == "__main__":
         model_name, device_map="auto", torch_dtype=bfloat16, attn_implementation="sdpa"
     )
     print("instatiated model")
-    run_name = "".join(random.choices(string.ascii_uppercase, k=5))
-    print("starting run", run_name)
-    for n_shots in 1, 4:
-        translate(
-            lang_pair="de-en",
-            n_shots=n_shots,
-            few_shot_dataset_name="wmt21",
-            test_dataset_name="wmt22",
-            out_dir=Path("outputs"),
-            run_name=run_name,
-            model=model,
-            tokenizer=tokenizer,
-            batch_size=8,
-        )
+    for formatter in [format_single_message_prompt_arrow, format_multi_message]:
+        run_name = "".join(random.choices(string.ascii_uppercase, k=4))
+        print("starting run", run_name)
+
+        for n_shots in [0, 1, 4]:
+            translate(
+                test=False,
+                lang_pair="de-en",
+                n_shots=n_shots,
+                prompt_formatter=formatter,
+                few_shot_dataset_name="wmt21",
+                test_dataset_name="wmt22",
+                out_dir=Path("outputs"),
+                run_name=run_name,
+                model=model,
+                tokenizer=tokenizer,
+                batch_size=8,
+            )
