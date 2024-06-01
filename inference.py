c412fb2551f184aa04e23d650da54d06bd1effc7
fynn.kroeger <fynn_kroeger@web.de>
2024-06-01 13:24:33 +0200

    black format

--- inference.py
+++ inference.py
@@ -7,7 +7,15 @@ from pathlib import Path
 import random
 
 
-def translate(lang_pair, n_shots, few_shot_dataset_name,test_dataset_name, model, tokenizer, batch_size):
+def translate(
+    lang_pair,
+    n_shots,
+    few_shot_dataset_name,
+    test_dataset_name,
+    model,
+    tokenizer,
+    batch_size,
+):
     source_lang, target_lang = lang_pair.split("-")
     prompt = f"Translate this from {LANG_TABLE[source_lang]} to {LANG_TABLE[target_lang]}:\n"
     one_message = 1
@@ -32,10 +40,14 @@ def translate(lang_pair, n_shots, few_shot_dataset_name,test_dataset_name, model
         else:
             few_shot_prompt_messages = []
             for sample in few_shot_dataset[:n_shots]:
-                few_shot_prompt_messages.append(dict(role="user", content=prompt + sample["source"]))
+                few_shot_prompt_messages.append(
+                    dict(role="user", content=prompt + sample["source"])
+                )
                 few_shot_prompt_messages.append(dict(role="assistant", content=sample["target"]))
 
-        out_path = Path(f"outputs/{test_dataset_name}_{lang_pair}_{n_shots}shot_{few_shot_dataset_name}_rand{'_'+str(one_message) if one_message else ''}.json")
+        out_path = Path(
+            f"outputs/{test_dataset_name}_{lang_pair}_{n_shots}shot_{few_shot_dataset_name}_rand{'_'+str(one_message) if one_message else ''}.json"
+        )
     else:
         out_path = Path(f"outputs/{test_dataset_name}_{lang_pair}_0shot.json")
     print(out_path)
@@ -45,26 +57,61 @@ def translate(lang_pair, n_shots, few_shot_dataset_name,test_dataset_name, model
         test_dataset = json.load(f)
     output = []
     for i in tqdm(range(0, len(test_dataset), batch_size)):
-        batch = test_dataset[i:i + batch_size]
+        batch = test_dataset[i : i + batch_size]
         messages2d = []
         for sample in batch:
             if one_message == 1:
-                messages2d.append([{"role": "user", "content": few_shot_prompt_string + sample['source'] + " -> "}])
+                messages2d.append(
+                    [
+                        {
+                            "role": "user",
+                            "content": few_shot_prompt_string + sample["source"] + " -> ",
+                        }
+                    ]
+                )
             elif one_message == 2:
-                messages2d.append([{"role": "user", "content": few_shot_prompt_string + f"{LANG_TABLE[source_lang]}: {sample['source']}\n{LANG_TABLE[target_lang]}: "}])
+                messages2d.append(
+                    [
+                        {
+                            "role": "user",
+                            "content": few_shot_prompt_string
+                            + f"{LANG_TABLE[source_lang]}: {sample['source']}\n{LANG_TABLE[target_lang]}: ",
+                        }
+                    ]
+                )
             else:
-                sample_prompt_message = [{"role": "user", "content": prompt+sample["source"]}]
-                messages2d.append(few_shot_prompt_messages+sample_prompt_message)
+                sample_prompt_message = [{"role": "user", "content": prompt + sample["source"]}]
+                messages2d.append(few_shot_prompt_messages + sample_prompt_message)
         if i == 0:
             print(messages2d[0])
-        model_inputs = tokenizer.apply_chat_template(messages2d, padding=True, return_tensors="pt", tokenize=True, add_generation_prompt=True)
+        model_inputs = tokenizer.apply_chat_template(
+            messages2d,
+            padding=True,
+            return_tensors="pt",
+            tokenize=True,
+            add_generation_prompt=True,
+        )
         input_sequence_len = model_inputs.shape[1]
         model_inputs = model_inputs.to("cuda")
-        generation = model.generate(model_inputs, max_new_tokens=512, pad_token_id=tokenizer.eos_token_id, return_dict_in_generate=True)
+        generation = model.generate(
+            model_inputs,
+            max_new_tokens=512,
+            pad_token_id=tokenizer.eos_token_id,
+            return_dict_in_generate=True,
+        )
         new_tokens = generation.sequences[:, input_sequence_len:]
-        decoded = tokenizer.batch_decode(new_tokens, skip_special_tokens=True,)
+        decoded = tokenizer.batch_decode(
+            new_tokens,
+            skip_special_tokens=True,
+        )
         for translation, sample in zip(decoded, batch):
-            output.append(dict(source=sample["source"], target=sample["target"], translation=translation))
+            output.append(
+                dict(
+                    source=sample["source"],
+                    target=sample["target"],
+                    translation=translation,
+                )
+            )
             print(output[-1])
 
         with open(out_path, "w") as f:
@@ -75,7 +122,17 @@ if __name__ == "__main__":
     model_name = "mistralai/Mistral-7B-Instruct-v0.2"
     tokenizer = AutoTokenizer.from_pretrained(model_name, padding_side="left")
     tokenizer.pad_token_id = tokenizer.eos_token_id
-    model = AutoModelForCausalLM.from_pretrained(model_name, device_map="auto", torch_dtype=bfloat16, attn_implementation="sdpa")
+    model = AutoModelForCausalLM.from_pretrained(
+        model_name, device_map="auto", torch_dtype=bfloat16, attn_implementation="sdpa"
+    )
     print("instatiated model")
 
-    translate(lang_pair="de-en", n_shots=4,few_shot_dataset_name = "wmt21",test_dataset_name = "wmt22", model=model, tokenizer=tokenizer, batch_size=8)
+    translate(
+        lang_pair="de-en",
+        n_shots=4,
+        few_shot_dataset_name="wmt21",
+        test_dataset_name="wmt22",
+        model=model,
+        tokenizer=tokenizer,
+        batch_size=8,
+    )
