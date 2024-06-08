4447b2a87b0a6b41e00aec32cb3e2ae1508104a8
fynn.kroeger <fynn_kroeger@web.de>
2024-06-08 16:53:59 +0200

    multiprocessing for heatmap

--- heatmap.py
+++ heatmap.py
@@ -1,39 +1,51 @@
 import json
-import pickle
 import numpy as np
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 from pathlib import Path
+from multiprocessing import Pool, cpu_count
 
-n_shots = 4
-for path in sorted(Path("attentions").iterdir()):
-    if path.suffix != ".npy":
-        continue
+
+def process_layer(args):
+    path, layer_index, tokens, attention, labelsize, figsize = args
+    seqlen = attention.shape[-1]
+    fig, ax = plt.subplots(figsize=(figsize, figsize))
+    ax.set_title(f"Layer {layer_index}")
+    ax.imshow(attention[layer_index], cmap="viridis")
+    ax.set_xticks(np.arange(seqlen))
+    ax.set_yticks(np.arange(seqlen))
+    ax.set_xticklabels(tokens)
+    ax.set_yticklabels(tokens)
+    ax.tick_params(axis="x", labelrotation=90)
+    ax.tick_params(labelsize=labelsize)
+    ax.spines["top"].set_visible(False)
+    ax.spines["bottom"].set_visible(False)
+    ax.spines["left"].set_visible(False)
+    ax.spines["right"].set_visible(False)
+    # fig.colorbar(im, ax=ax)
+
+    out_path = Path(f"attentions/heatmaps/{path.stem}/layer{layer_index}.png")
+    out_path.parent.mkdir(exist_ok=True, parents=True)
+    plt.savefig(out_path, dpi=200, bbox_inches="tight")
+    plt.close(fig)
+
+
+def process_file(path, n_shots):
     attention = np.load(path)
     with open(path.with_suffix(".json")) as f:
         tokens = json.load(f)
     labelsize = {0: 10, 1: 7, 2: 6, 4: 6}[n_shots]
     figsize = {0: 10, 1: 10, 2: 32, 4: 50}[n_shots]
-    layers = [0, 1, 2, 14, 15, 16, 29, 30, 31]
     layers = list(range(32))
-    seqlen = attention.shape[-1]
-    for layer_index in tqdm(layers):
-        fig, ax = plt.subplots(figsize=(figsize, figsize))
-        ax.set_title(f"Layer {layer_index}")
-        ax.imshow(attention[layer_index], cmap="viridis")
-        ax.set_xticks(np.arange(seqlen))
-        ax.set_yticks(np.arange(seqlen))
-        ax.set_xticklabels(tokens)
-        ax.set_yticklabels(tokens)
-        ax.tick_params(axis="x", labelrotation=90)
-        ax.tick_params(labelsize=labelsize)
-        ax.spines["top"].set_visible(False)
-        ax.spines["bottom"].set_visible(False)
-        ax.spines["left"].set_visible(False)
-        ax.spines["right"].set_visible(False)
-        # fig.colorbar(im, ax=ax)
+    args = [(path, layer_index, tokens, attention, labelsize, figsize) for layer_index in layers]
 
-        out_path = Path(f"attentions/heatmaps/{path.stem}/layer{layer_index}.png")
-        out_path.parent.mkdir(exist_ok=True, parents=True)
-        plt.savefig(out_path, dpi=200, bbox_inches="tight")
-        plt.close(fig)
+    with Pool(cpu_count() - 4) as pool:
+        list(tqdm(pool.imap_unordered(process_layer, args), total=len(layers)))
+
+
+if __name__ == "__main__":
+    n_shots = 4
+    for path in sorted(Path("attentions").iterdir()):
+        if path.suffix != ".npy":
+            continue
+        process_file(path, n_shots)
