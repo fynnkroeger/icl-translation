22a0105dc202ab9e66597a08054cf4a6b72dff53
fynn.kroeger <fynn_kroeger@web.de>
2024-06-15 19:44:36 +0200

    working attention flow function

--- attention_flow.py
+++ attention_flow.py
@@ -10,8 +10,36 @@ with open(path.with_suffix(".json")) as f:
     tokens = json.load(f)
 
 print(attention.shape)
+# for i, t in enumerate(tokens):
+#     print(i, t)
 
+start_and_inst = [0, 1, 2, 3]
 
+source1 = list(range(4, 24))
+end_source1 = [24, 25]  # . ->
+target1 = list(range(26, 43))
+end_target1 = [43, 44]  # . ###
+
+source2 = list(range(45, 62))
+end_source2 = [62]  # ->
+target2 = list(range(63, 74))
+end_target2 = [74, 75]  # . ###
+
+# clean " ?
+source3 = list(range(76, 91))
+end_source3 = [91, 92]  # . ->
+target3 = list(range(93, 103))
+end_target3 = [103, 104]  # . ###
+
+source4 = list(range(105, 129))
+end_source4 = [129, 130]  # . ->
+target4 = list(range(131, 146))
+end_target4 = [146, 147]  # . ###
+
+task_source = list(range(148, 160))
+task_source_end = [160, 161]  # . ->
+inst = [162, 163, 164, 165, 166]
+task_target = list(range(167, 181))
 """
 kinds of tokens:
 - instruction
@@ -37,3 +65,12 @@ interesting patterns
 - source to target
 - between summary tokens
 """
+
+
+def attention_flow(from_tokens, to_tokens, matrix):
+    coordinates = [(i, j) for i in from_tokens for j in to_tokens]
+    flow = sum(matrix[:, j, i] for i, j in coordinates)
+    return flow / len(coordinates)
+
+
+print(attention_flow(start_and_inst, source1, attention))
