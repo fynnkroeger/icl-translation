e122f1914f3d8c9a05fb9efb39b37a9d2640b177
fynn.kroeger <fynn_kroeger@web.de>
2024-06-22 10:47:41 +0000

    improvement on attention flow

--- attention_flow.py
+++ attention_flow.py
@@ -16,26 +16,13 @@ print(attention.shape)
 
 start_and_inst = [0, 1, 2, 3]
 
-source1 = list(range(4, 24))
-end_source1 = [24, 25]  # . ->
-target1 = list(range(26, 43))
-end_target1 = [43, 44]  # . ###
+source = [list(range(4, 24)), list(range(45, 62)), list(range(76, 91)), list(range(105, 129))]
 
-source2 = list(range(45, 62))
-end_source2 = [62]  # ->
-target2 = list(range(63, 74))
-end_target2 = [74, 75]  # . ###
+end_source = [[24, 25], [62], [91, 92], [129, 130]]
 
-# clean " ?
-source3 = list(range(76, 91))
-end_source3 = [91, 92]  # . ->
-target3 = list(range(93, 103))
-end_target3 = [103, 104]  # . ###
+target = [list(range(26, 43)), list(range(63, 74)), list(range(93, 103)), list(range(131, 146))]
 
-source4 = list(range(105, 129))
-end_source4 = [129, 130]  # . ->
-target4 = list(range(131, 146))
-end_target4 = [146, 147]  # . ###
+end_target = [[43, 44], [74, 75], [103, 104], [146, 147]]
 
 task_source = list(range(148, 160))
 task_source_end = [160, 161]  # . ->
@@ -70,23 +57,45 @@ interesting patterns
 """
 
 
+def flat(a):
+    out = []
+    for x in a:
+        out += x
+    return out
+
+
 def coords(from_tokens, to_tokens):
     return [(i, j) for i in from_tokens for j in to_tokens if i < j]
 
 
-sources = source1 + source2 + source3 + source4
-targets = target1 + target2 + target3 + target4
-end_sources = end_source1 + end_source2 + end_source3 + end_source4
-end_targets = end_target1 + end_target2 + end_target3 + end_target4
-examples = sources + targets + end_sources + end_targets
+def coords_multi(from_tokens, to_tokens):
+    out = []
+    for a, b in zip(from_tokens, to_tokens):
+        out += coords(a, b)
+    return out
+
+
 coordinates = {}
 # seperate examples and task??
-coordinates["source-source_end"] = coords(sources, end_sources)
-coordinates["source-target"] = coords(sources + end_sources, targets)
-coordinates["end_targets-inst"] = coords(end_targets, inst)
+coordinates["source-source_end"] = coords_multi(source, end_source)
+coordinates["source-target"] = coords_multi(source, target)
+coordinates["source_end-target"] = coords_multi(end_source, target)
+coordinates["source_end-target_end"] = coords_multi(end_source, end_target)
+
+coordinates["end_target-inst"] = coords(flat(end_target), inst)
 
 coordinates["inst-task_target"] = coords(inst, task_target)
-coordinates["rest"] = [(i, j) for i in range(len(tokens)) for j in range(len(tokens)) if i < j]
+coordinates[""]
+
+everything = []
+for v in coordinates.values():
+    everything += v
+coordinates["rest"] = [
+    (i, j)
+    for i in range(len(tokens))
+    for j in range(len(tokens))
+    if i < j and (i, j) not in everything
+]
 
 flows = {}
 for key, c in coordinates.items():
@@ -95,6 +104,6 @@ for key, c in coordinates.items():
 for k, v in flows.items():
     plt.plot(v, label=k)
 plt.legend()
-plt.savefig("out.png")
+plt.savefig("out.png", dpi=300)
 
 print(flows)
