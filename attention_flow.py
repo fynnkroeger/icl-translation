3d0ac040f3a9686c52ec0e481eaa0f097acd2444
fynn.kroeger <fynn_kroeger@web.de>
2024-06-22 13:56:07 +0200

    more attention flows

--- attention_flow.py
+++ attention_flow.py
@@ -3,11 +3,30 @@ from pathlib import Path
 import json
 import matplotlib.pyplot as plt
 
+
+def flat(a):
+    out = []
+    for x in a:
+        out += x
+    return out
+
+
+def coords(from_tokens, to_tokens):
+    return [(i, j) for i in from_tokens for j in to_tokens if i < j]
+
+
+def coords_multi(from_tokens, to_tokens):
+    out = []
+    for a, b in zip(from_tokens, to_tokens):
+        out += coords(a, b)
+    return out
+
+
 # todo use avg pooling here
 # todo change naming for attentions (format, index), put all in folder if we do more
-path = Path("attentions/wmt22_de-en_04shot_wmt21_IDIS_00.npy")
+path = Path("attentions/wmt22_de-en_04shot_wmt21_format_single_message_arrow_oneline_00_avg.npy")
 attention = np.load(path)
-with open(path.with_suffix(".json")) as f:
+with open(path.with_stem(path.stem[:-4]).with_suffix(".json")) as f:
     tokens = json.load(f)
 
 print(attention.shape)
@@ -57,35 +76,20 @@ interesting patterns
 """
 
 
-def flat(a):
-    out = []
-    for x in a:
-        out += x
-    return out
-
-
-def coords(from_tokens, to_tokens):
-    return [(i, j) for i in from_tokens for j in to_tokens if i < j]
-
-
-def coords_multi(from_tokens, to_tokens):
-    out = []
-    for a, b in zip(from_tokens, to_tokens):
-        out += coords(a, b)
-    return out
-
-
 coordinates = {}
 # seperate examples and task??
-coordinates["source-source_end"] = coords_multi(source, end_source)
-coordinates["source-target"] = coords_multi(source, target)
-coordinates["source_end-target"] = coords_multi(end_source, target)
-coordinates["source_end-target_end"] = coords_multi(end_source, end_target)
+# coordinates["source-source_end"] = coords_multi(source, end_source)
+# coordinates["source-target"] = coords_multi(source, target)
+# coordinates["source_end-target"] = coords_multi(end_source, target)
+# coordinates["source_end-target_end"] = coords_multi(end_source, end_target)
+coordinates["source_target"] = coords(task_source, task_target)
+# coordinates["source_source-end"] = coords(task_source, task_source_end)
+coordinates["source_inst"] = coords(task_source, inst)
+coordinates["source-end_inst"] = coords(task_source_end, inst)
 
-coordinates["end_target-inst"] = coords(flat(end_target), inst)
+# coordinates["end_target-inst"] = coords(flat(end_target), inst)
 
 coordinates["inst-task_target"] = coords(inst, task_target)
-coordinates[""]
 
 everything = []
 for v in coordinates.values():
@@ -96,10 +100,10 @@ coordinates["rest"] = [
     for j in range(len(tokens))
     if i < j and (i, j) not in everything
 ]
-
+del coordinates["rest"]
 flows = {}
 for key, c in coordinates.items():
-    flows[key] = sum(attention[:, j, i] for i, j in c) / len(c)
+    flows[key] = sum(attention[:, j, i] for i, j in c)  # / len(c)
 
 for k, v in flows.items():
     plt.plot(v, label=k)
