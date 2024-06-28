535e55e3aaa9864640c342958ed4ab9a5a118a32
fynn.kroeger <fynn_kroeger@web.de>
2024-06-28 16:32:49 +0200

    add prompt print util

--- utils.py
+++ utils.py
@@ -12,3 +12,22 @@ LANG_TABLE = {
     "ro": "Romanian",
     "gu": "Gujarati",
 }
+
+if __name__ == "__main__":
+    from inference import *
+
+    few = [
+        dict(source="Guten Morgen!", target="Good Morning!"),
+        dict(source="Fische schwimmen", target="Fish are swimming"),
+    ]
+    for func in [
+        format_single_message_arrow_title,
+        format_single_message_labeled,
+        format_single_message_arrow,
+        format_single_message_arrow_oneline,
+    ]:
+        prompt = func(few, "Ich mag Wasser.", "German", "English")
+        text = prompt[-1]["content"]
+        print(text)
+        print()
+        print()
