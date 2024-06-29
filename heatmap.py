68ada486cdc2b947a155c056d326b28f45f28b5b
fynn.kroeger <fynn_kroeger@web.de>
2024-06-29 14:48:53 +0200

    update heatmap creation

--- heatmap.py
+++ heatmap.py
@@ -40,11 +40,14 @@ def process_file(path, index, n_shots):
     layers = list(range(32))
     args = [(path, layer_index, tokens, attention, labelsize, figsize) for layer_index in layers]
 
-    with Pool(8) as pool:
+    with Pool(16) as pool:
         list(tqdm(pool.imap_unordered(process_layer, args), total=len(layers)))
 
 
 if __name__ == "__main__":
     for path in Path("Mistral-7B-v0.1/attention").iterdir():
         print(path)
-        process_file(path, index=0, n_shots=2)
+        n_shots = int(path.name.split("_")[2][:2])
+        if n_shots == 4:
+            n_shots = 2
+        process_file(path, index=0, n_shots=n_shots)
