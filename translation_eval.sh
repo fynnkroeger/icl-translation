17bbcc1c659aba4f86f0e177dde0bc31c3c2ba78
fynn.kroeger <fynn_kroeger@web.de>
2024-07-05 11:47:48 +0200

    update batch file

--- translation_eval.sh
+++ translation_eval.sh
@@ -5,10 +5,10 @@
 #SBATCH --gpus 1
 #SBATCH --mem 32G
 #SBATCH --cpus-per-task=16
-#SBATCH --job-name "icl_translation_eval"
+#SBATCH --job-name "icl_translation_attention"
 #SBATCH --output sbatch_out.txt
-#SBATCH --time 4:00:00
+#SBATCH --time 8:00:00
 source "/hpi/fs00/home/fynn.kroeger/miniconda3/etc/profile.d/conda.sh"
 conda activate alma-r
 cd /hpi/fs00/home/fynn.kroeger/project/icl-translation
-python inference.py
+python attention2.py
